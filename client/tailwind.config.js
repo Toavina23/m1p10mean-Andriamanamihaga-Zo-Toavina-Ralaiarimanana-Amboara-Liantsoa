6cfdbd31221ddeec42f794d0fe0642f5ea4d4a08
Amboara01 <amboara.ralaiarimanana@outlook.com>
2024-02-14 13:50:35 +0300

    Setup spartan

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
-    "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts}',
+    './libs/ui/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
