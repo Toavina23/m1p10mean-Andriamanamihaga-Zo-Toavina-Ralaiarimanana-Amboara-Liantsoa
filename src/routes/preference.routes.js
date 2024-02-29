1f5991da3a320aa8dfb37b9cafc73ad10f1807eb
Amboara01 <amboara.ralaiarimanana@outlook.com>
2024-02-29 21:39:25 +0300

    Update preference.routes.js

--- src/routes/preference.routes.js
+++ src/routes/preference.routes.js
@@ -13,37 +13,31 @@ const preferencesRouter = express.Router();
 preferencesRouter.post(
 	"/services",
 	isAuthenticated,
-	isCustomer,
 	setToPreferedService
 );
 preferencesRouter.delete(
 	"/services",
 	isAuthenticated,
-	isCustomer,
 	removeFromPreferedService
 );
 preferencesRouter.get(
 	"/services",
 	isAuthenticated,
-	isCustomer,
 	getUserPreferedServices
 );
 preferencesRouter.post(
 	"/employees",
 	isAuthenticated,
-	isCustomer,
 	setToPreferedEmployee
 );
 preferencesRouter.delete(
 	"/employees",
 	isAuthenticated,
-	isCustomer,
 	removeFromPreferedEmployee
 );
 preferencesRouter.get(
 	"/employees",
 	isAuthenticated,
-	isCustomer,
 	getUserPreferedEmployees
 );
 module.exports = preferencesRouter;
